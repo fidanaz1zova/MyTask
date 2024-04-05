d90715d4d5c349ad80883f5682e6fc1fa157ec3b
Fidan Azizova <161811741+fidanaz1zova@users.noreply.github.com>
2024-04-05 13:01:56 +0400

    Add files via upload

---
+++ classtsk-7/app.js
@@ -0,0 +1,349 @@
+let students = [
+    {
+    id: 1,
+    name: "Nurane",
+    surname: "Ismayilzade",
+    age: 21,
+    hobbies: ["music", "walking", "watchingfilm"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "cavid" },
+    { id: 4, name: "alisa" },
+    ],
+    loginDetail: { username: "nurana21", password: "nunu123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 88,
+    salaryAZN: 144,
+    university: "BDU",
+    },
+    {
+    id: 2,
+    name: "Gizilgul",
+    surname: "Allahverdiyeva",
+    age: 20,
+    hobbies: ["book", "writing code"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "babaxan" },
+    { id: 4, name: "gulshen" },
+    ],
+    loginDetail: { username: "allahverdieva1", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 70,
+    salaryAZN: 100,
+    university: "BDU",
+    },
+    {
+    id: 3,
+    name: "Xanim",
+    surname: "Nuriyeva",
+    age: 21,
+    hobbies: ["book", "music"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "hikmet" },
+    { id: 4, name: "gulsen" },
+    ],
+    loginDetail: { username: "xanim123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 145,
+    university: "BDU",
+    },
+    {
+    id: 4,
+    name: "Minaya",
+    surname: "Binnetova",
+    age: 21,
+    hobbies: ["book", "gaming", "movie", "music"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "anar" },
+    ],
+    loginDetail: { username: "binnetova", password: "hello123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 90,
+    salaryAZN: 142,
+    university: "BDU",
+    },
+    {
+    id: 5,
+    name: "Sabina",
+    surname: "Hatamli",
+    age: 21,
+    hobbies: ["shopping", "listen to music"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "Mirvari" },
+    ],
+    loginDetail: { username: "sebine123", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 81,
+    salaryAZN: 146,
+    university: "BDU",
+    },
+    
+    {
+    id: 6,
+    name: "Ləman",
+    surname: "Şamilova",
+    age: 20,
+    hobbies: [
+    "watching movies",
+    "reading books",
+    "painting",
+    " walking",
+    " listen to music",
+    ],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "lemanshamilova", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 85.8,
+    salaryAZN: 145,
+    university: "BDU",
+    },
+    {
+    id: 7,
+    name: "Narmin",
+    surname: "Musayeva",
+    age: 21,
+    hobbies: ["book", "gaming", "painting", "walking"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "fidan" },
+    { id: 4, name: "Irada" },
+    ],
+    loginDetail: {
+    username: "narmin0",
+    password: "narmin01",
+    },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 196,
+    university: "BDU",
+    },
+    
+    {
+    id: 8,
+    name: "Fatima",
+    surname: "Akhundzada",
+    age: 20,
+    hobbies: ["drawing", "sleeping"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "Valida" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 76,
+    salaryAZN: 100,
+    university: "BDU",
+    },
+    {
+    id: 9,
+    name: "Elmir",
+    surname: "Huseynov",
+    age: 21,
+    hobbies: ["book", "ice skating", "Tennis", "Karting"],
+    student: true,
+    teacher: [
+    { id: 1, name: "Gurban" },
+    { id: 2, name: "Hajar" },
+    { id: 3, name: "Morad" },
+    { id: 4, name: "Fikrat" },
+    ],
+    loginDetail: { username: "huseynovelmirr", password: "maxverstappen" },
+    gender: "man",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 120,
+    university: "BDU",
+    },
+    {
+    id: 10,
+    name: "Fidan",
+    surname: "Rehimli",
+    age: 21,
+    hobbies: ["book", "gaming"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "Turan" },
+    ],
+    loginDetail: { username: "fidan123", password: "fidanrahimli" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 75,
+    salaryAZN: 98,
+    university: "BDU",
+    },
+    {
+    id: 11,
+    name: "Aynur",
+    surname: "Aynurova",
+    age: 20,
+    hobbies: ["book", "gaming"],
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 81,
+    salaryAZN: 120,
+    university: "BDU",
+    },
+    {
+    id: 12,
+    name: "Elman",
+    surname: "Muradov",
+    age: 20,
+    hobbies: [
+    "book",
+    "gaming",
+    "sking",
+    "wrestling",
+    "swiming",
+    "arguing about philosophy",
+    "solving sudoku",
+    "hiking",
+    "walking",
+    ],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "jale" },
+    { id: 4, name: "akhmed" },
+    ],
+    loginDetail: { username: "elman17", password: "elman12345" },
+    gender: "boy",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 100,
+    university: "BDU",
+    },   
+    {
+        id: 13,
+        name: "Fidan",
+        surname: "Azizova",
+        age: 20,
+        hobbies: [
+        "reading",
+        "watching",
+        "shooting",
+        "drawing",
+        "cooking",
+        "walking",
+        ],
+        student: true,
+        teacher: [
+        { id: 1, name: "gurban" },
+        { id: 2, name: "hajar" },
+        { id: 3, name: "afag" },
+        { id: 4, name: "tarana" },
+        ],
+        loginDetail: { username: "fidanaz1zova", password: "fidan2004" },
+        gender: "girl",
+        boyfriendGirlfriend: false,
+        fail: false,
+        avgPoint: 92,
+        salaryAZN: 172,
+        university: "BDU",
+        },
+    ];
+
+    // Task 1
+    // console.log(students)
+
+    // Task 2
+    // for (i=0; i<students.length; i++){
+    //     console.log(students[i].name)
+    // }
+
+    // Task 3
+    // for (i=0; i<students.length; i++){
+    //     console.log(students[i].name, students[i].surname )
+    // }
+
+    // Task 4
+    // for (i=0; i<students.length; i++){
+    //   if(students[i].fail == true){
+    //        console.log(students[i].name, students[i].surname)
+    //   }
+    // }
+
+    // Task 5
+    // for (i=0; i<students.length; i++){
+    //     if(students[i].hobbies.length == ){
+    //   console.log(students[i].name, students[i].surname)
+    //   }
+    // }
+
+    // Task 6
+      for (i=0; i<students.length; i++){
+      console.log(Math.max(...students[i].avgPoint));
+      }
+    
+   
+
+    // Task 7
+
+
+    // Task 8
+    // for (i=0; i<students.length; i++){
+    //     if(students[i].boyfriendGirlfriend == true){
+    //   console.log(students[i].name, students[i].surname)
+    //   }
+    // }
+
+    // Task 9
+    // for (i=0; i<students.length; i++){
+    //     if(students[i].age == 20){
+    //   console.log(students[i].name, students[i].surname)
+    //   }
+    // }
+    
+
+
+
