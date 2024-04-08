afc020eaf3d5f68332f4111e351973c8afb29c97
Fidan Azizova <161811741+fidanaz1zova@users.noreply.github.com>
2024-04-08 13:39:08 +0400

    aaaagggggggggg

---
+++ classtsk-8/app.js
@@ -0,0 +1,384 @@
+let students = [
+    {
+    id: 1,
+    name: "Nurane",
+    surname: "Ismayilzade",
+    age: 21,
+    hobbies: ["music", "walking", "watchingfilm"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "cavid" },
+    { id: 4, name: "alisa" },
+    ],
+    loginDetail: { username: "nurana21", password: "nunu123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 88,
+    salaryAZN: 144,
+    university: "BDU",
+    },
+    {
+    id: 2,
+    name: "Gizilgul",
+    surname: "Allahverdiyeva",
+    age: 20,
+    hobbies: ["book", "writing code"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "babaxan" },
+    { id: 4, name: "gulshen" },
+    ],
+    loginDetail: { username: "allahverdieva1", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 70,
+    salaryAZN: 100,
+    university: "BDU",
+    },
+    {
+    id: 3,
+    name: "Xanim",
+    surname: "Nuriyeva",
+    age: 21,
+    hobbies: ["book", "music"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "hikmet" },
+    { id: 4, name: "gulsen" },
+    ],
+    loginDetail: { username: "xanim123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 145,
+    university: "BDU",
+    },
+    {
+    id: 4,
+    name: "Minaya",
+    surname: "Binnetova",
+    age: 21,
+    hobbies: ["book", "gaming", "movie", "music"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "anar" },
+    ],
+    loginDetail: { username: "binnetova", password: "hello123" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 90,
+    salaryAZN: 142,
+    university: "BDU",
+    },
+    {
+    id: 5,
+    name: "Sabina",
+    surname: "Hatamli",
+    age: 21,
+    hobbies: ["shopping", "listen to music"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "Mirvari" },
+    ],
+    loginDetail: { username: "sebine123", password: "salam123" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 81,
+    salaryAZN: 146,
+    university: "BDU",
+    },
+    
+    {
+    id: 6,
+    name: "Ləman",
+    surname: "Şamilova",
+    age: 20,
+    hobbies: [
+    "watching movies",
+    "reading books",
+    "painting",
+    " walking",
+    " listen to music",
+    ],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "lemanshamilova", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 85.8,
+    salaryAZN: 145,
+    university: "BDU",
+    },
+    {
+    id: 7,
+    name: "Narmin",
+    surname: "Musayeva",
+    age: 21,
+    hobbies: ["book", "gaming", "painting", "walking"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "fidan" },
+    { id: 4, name: "Irada" },
+    ],
+    loginDetail: {
+    username: "narmin0",
+    password: "narmin01",
+    },
+    gender: "girl",
+    boyfriendGirlfriend: false,
+    fail: false,
+    avgPoint: 80,
+    salaryAZN: 196,
+    university: "BDU",
+    },
+    
+    {
+    id: 8,
+    name: "Fatima",
+    surname: "Akhundzada",
+    age: 20,
+    hobbies: ["drawing", "sleeping"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "Valida" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: false,
+    avgPoint: 76,
+    salaryAZN: 100,
+    university: "BDU",
+    },
+    {
+    id: 9,
+    name: "Elmir",
+    surname: "Huseynov",
+    age: 21,
+    hobbies: ["book", "ice skating", "Tennis", "Karting"],
+    student: true,
+    teacher: [
+    { id: 1, name: "Gurban" },
+    { id: 2, name: "Hajar" },
+    { id: 3, name: "Morad" },
+    { id: 4, name: "Fikrat" },
+    ],
+    loginDetail: { username: "huseynovelmirr", password: "maxverstappen" },
+    gender: "man",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 120,
+    university: "BDU",
+    },
+    {
+    id: 10,
+    name: "Fidan",
+    surname: "Rehimli",
+    age: 21,
+    hobbies: ["book", "gaming"],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "Turan" },
+    ],
+    loginDetail: { username: "fidan123", password: "fidanrahimli" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 75,
+    salaryAZN: 98,
+    university: "BDU",
+    },
+    {
+    id: 11,
+    name: "Aynur",
+    surname: "Aynurova",
+    age: 20,
+    hobbies: ["book", "gaming"],
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    ],
+    loginDetail: { username: "aynur123", password: "salamBaku" },
+    gender: "girl",
+    boyfriendGirlfriend: true,
+    fail: true,
+    avgPoint: 81,
+    salaryAZN: 120,
+    university: "BDU",
+    },
+    {
+    id: 12,
+    name: "Elman",
+    surname: "Muradov",
+    age: 20,
+    hobbies: [
+    "book",
+    "gaming",
+    "sking",
+    "wrestling",
+    "swiming",
+    "arguing about philosophy",
+    "solving sudoku",
+    "hiking",
+    "walking",
+    ],
+    student: true,
+    teacher: [
+    { id: 1, name: "gurban" },
+    { id: 2, name: "hajar" },
+    { id: 3, name: "jale" },
+    { id: 4, name: "akhmed" },
+    ],
+    loginDetail: { username: "elman17", password: "elman12345" },
+    gender: "boy",
+    boyfriendGirlfriend: false,
+    fail: true,
+    avgPoint: 76,
+    salaryAZN: 100,
+    university: "BDU",
+    },   
+    {
+        id: 13,
+        name: "Fidan",
+        surname: "Azizova",
+        age: 20,
+        hobbies: [
+        "reading",
+        "watching",
+        "shooting",
+        "drawing",
+        "cooking",
+        "walking",
+        ],
+        student: true,
+        teacher: [
+        { id: 1, name: "gurban" },
+        { id: 2, name: "hajar" },
+        { id: 3, name: "afag" },
+        { id: 4, name: "tarana" },
+        ],
+        loginDetail: { username: "fidanaz1zova", password: "fidan2004" },
+        gender: "girl",
+        boyfriendGirlfriend: false,
+        fail: false,
+        avgPoint: 92,
+        salaryAZN: 172,
+        university: "BDU",
+        },
+    ];
+
+// Task 1
+// let allStudents = ( arr ) => {
+//  for (i=0; i<arr.length; i++){
+//   console.log(arr[i])
+//    }
+//  }
+// allStudents(students)
+
+
+// students.forEach((elem,  index, array) => {
+//     console.log(elem, index, array);
+// })
+
+// Task 2
+    // let newStudent = students.flat();
+    // console.log( newStudent);
+
+
+    // let newArr=students.map((element) => {
+    //     return element;
+    // })
+    // console.log(newArr)
+
+// Task 3
+// let newArr=students.map((element) => {
+//     return element.name;
+// })
+// console.log(newArr)
+
+// Task 4
+// let newArr=students.map((element) => {
+//  let nameAge= element.name + "-" + element.age;
+//     return nameAge;
+// })
+// console.log(newArr)
+
+// Task 5
+// let newArr=students.map((element) => {
+//  let obj = {};
+//  obj.name = element.name;
+//  obj.surname = element.surname;
+//  obj.age = element.age;
+//  obj.year = "2024" - element.age;
+//     return obj;
+// })
+// console.log(newArr)
+
+// Task 6
+// let newArr=students.filter((element) =>  element.age == 20)
+// console.log(newArr)
+
+// Task 7
+// let newArr=students.filter((element) =>  element.avgPoint > 80)
+// console.log(newArr)
+
+// Task 8
+// let newArr=students.filter((element) =>  element.salaryAZN < 100)
+// console.log(newArr)
+
+// Task 9
+// let newArr=students.filter((element) =>  element.hobbies.length > 5)
+// console.log(newArr)
+
+// Task 10
+// let newArr=students.filter((element) => element.loginDetail.password.includes(1)).map(element =>{
+//     return element.loginDetail.username
+// })
+// console.log(newArr)
+
+// Task 11
+// let numbers=[5, -3, 8, -9, 2];
+// let sum = 0;
+// numbers.forEach((element) => {
+//     sum += element;
+// })
+// console.log(sum)
+
+// Task 12
+// let newArr=students.reduce((acc, element) => {
+//     acc.push(element.name)
+//     return acc;
+// },[])
+// console.log("newArr", newArr)
+
+// Task 13
+// let newArr=students.filter((element) => element.name == "Xanim").map(element =>{
+//     return element
+// })
+// console.log(newArr)
